b37fbb4b214fad753c18bfdd5083b7ff6b04df90
wu233moji <1944323466@qq.com>
2017-11-09 18:13:17 +0900

    不知道为什么本地端文件没了
    copy++

--- Quick_sort.cpp
+++ Quick_sort.cpp
@@ -1,23 +1,21 @@
 #include <stdio.h> 
-int a[101],n;//定义全局变量，这两个变量需要在子函数中使用 
+int a[101],n;
 void quicksort(int left,int right) 
 { 
     int i,j,t,temp; 
     if(left>right) 
        return; 
                                 
-    temp=a[left]; //temp中存的就是基准数 
+    temp=a[left]; 
     i=left; 
     j=right; 
     while(i!=j) 
     { 
-                   //顺序很重要，要先从右边开始找 
+                   
                    while(a[j]>=temp && i<j) 
-                            j--; 
-                   //再找右边的 
+                            j--;
                    while(a[i]<=temp && i<j) 
-                            i++; 
-                   //交换两个数在数组中的位置 
+                            i++;  
                    if(i<j) 
                    { 
                             t=a[i]; 
@@ -25,23 +23,20 @@ void quicksort(int left,int right)
                             a[j]=t; 
                    } 
     } 
-    //最终将基准数归位 
+    
     a[left]=a[i]; 
     a[i]=temp; 
                              
-    quicksort(left,i-1);//继续处理左边的，这里是一个递归的过程 
-    quicksort(i+1,right);//继续处理右边的 ，这里是一个递归的过程 
+    quicksort(left,i-1);
+    quicksort(i+1,right);
 } 
 int main() 
 { 
     int i,j,t; 
-    //读入数据 
     scanf("%d",&n); 
     for(i=1;i<=n;i++) 
                    scanf("%d",&a[i]); 
-    quicksort(1,n); //快速排序调用 
-                             
-    //输出排序后的结果 
+    quicksort(1,n);
     for(i=1;i<=n;i++) 
         printf("%d ",a[i]); 
     getchar();getchar(); 
